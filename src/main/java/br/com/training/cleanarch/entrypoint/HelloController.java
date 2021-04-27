342a338a4c75a04a58a3ba10c1f475c995da74c2
Gabriel <gsstrozi@gmail.com>
2021-04-27 13:50:23 -0300

    Setup Project

--- src/main/java/br/com/training/cleanarch/entrypoint/HelloController.java
+++ src/main/java/br/com/training/cleanarch/entrypoint/HelloController.java
@@ -2,12 +2,21 @@ package br.com.training.cleanarch.entrypoint;
 
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class HelloController {
+
+    @Autowired
+    @Qualifier("dbJdbcTemplate")
+    NamedParameterJdbcTemplate dbJdbcTemplate;
 
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Retorna uma mensagem de sucesso"),
@@ -16,6 +25,8 @@ public class HelloController {
     })
     @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
     public String hello() {
+        dbJdbcTemplate.update("INSERT INTO TEST_INTEGRATION_SCRIPT VALUES ('TESTE 1')", new HashMap<String, String>());
+
         return "Hello, this is my first controller";
     }
 
